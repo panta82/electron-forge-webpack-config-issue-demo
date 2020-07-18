93c2f564679cbd49098041acda191365edf178a6
Ivan Pantic <ivan@pantas.net>
2020-07-18 18:28:42 +0200

    Add breaking package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const fsExtra = require('fs-extra');
+console.log(fsExtra);
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
